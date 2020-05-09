1d2de4dbe408e6df978ea2d82cbb99875fab9dc8
Roman Valls Guimera <brainstorm@nopcode.org>
2020-05-09 22:50:43 +1000

    Fix cmake command and directory handling

--- ci/build.sh
+++ ci/build.sh
@@ -2,11 +2,8 @@
 
 echo "Building for OpenCV $OPENCV_VERSION-$OPENCV_OPNP_REV on: \n `uname -a` \n"
 
-cd opencv/opencv-$OPENCV_VERSION/target/linux/ARMv8
-cmake -D BUILD_SHARED_LIBS=OFF \
-#            -D WITH_EIGEN=OFF \
-#            -D WITH_FFMPEG=OFF \
-            -D WITH_JAVA=ON
+pushd opencv/opencv-$OPENCV_VERSION/target/linux/ARMv8
+cmake -D BUILD_SHARED_LIBS=OFF -D WITH_EIGEN=OFF -D WITH_FFMPEG=OFF -D WITH_JAVA=ON ../../..
 make -j8
-cd ../../.. && ./copy-resources $OPENCV_VERSION-$OPENCV_OPNP_REV
+popd && ./copy-resources $OPENCV_VERSION
 mvn clean test
