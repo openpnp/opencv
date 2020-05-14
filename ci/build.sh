654259912f00f47fbaa34fd965f1ea88774f731e
Roman Valls Guimera <brainstorm@nopcode.org>
2020-05-14 21:46:06 +1000

    pushd/popd not found, going manual with cd..

--- ci/build.sh
+++ ci/build.sh
@@ -2,8 +2,8 @@
 
 echo "Building for OpenCV $OPENCV_VERSION-$OPENCV_OPNP_REV on: \n `uname -a` \n"
 
-pushd opencv/opencv-$OPENCV_VERSION/target/linux/ARMv8
+cd opencv/opencv-$OPENCV_VERSION/target/linux/ARMv8
 cmake -D BUILD_SHARED_LIBS=OFF -D WITH_EIGEN=OFF -D WITH_FFMPEG=OFF -D WITH_JAVA=ON ../../..
 make -j8
-popd && ./copy-resources $OPENCV_VERSION
+cd ../../.. && ./copy-resources $OPENCV_VERSION
 mvn clean test
