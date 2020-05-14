354c0c2cdff3813c39eafbc2f6eac7b4081790dc
Roman Valls Guimera <brainstorm@nopcode.org>
2020-05-14 22:40:26 +1000

    Add more JNI/JNA juice for cmake, get a proper JVM like we do on openpnp-kiosk

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,18 @@
 #!/bin/sh -x
 
+MACHINE_NAME=`uname -m`
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/aarch64-linux-gnu/jni
+export JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto
+export ANT_HOME=/usr/bin/ant
+
 echo "Building for OpenCV $OPENCV_VERSION-$OPENCV_OPNP_REV on: \n `uname -a` \n"
+
+# Apply JNI Debian hacks, avoids OpenCV's cmake going:
+# "Could NOT find JNI (missing: JAVA_AWT_LIBRARY JAVA_JVM_LIBRARY JAVA_INCLUDE_PATH ...)"
+ln -sf /usr/lib/$MACHINE_NAME-linux-gnu/jni/libjnidispatch.system.so /usr/lib/$MACHINE_NAME-linux-gnu/jni/ libjnidispatch.so
+
+# Get and install a proper JVM
+wget https://corretto.aws/downloads/latest/amazon-corretto-11-$MACHINE_NAME-linux-jdk.deb && dpkg -i *.deb && rm *.deb
 
 cd opencv/opencv-$OPENCV_VERSION/target/linux/ARMv8
 cmake -D BUILD_SHARED_LIBS=OFF -D WITH_EIGEN=OFF -D WITH_FFMPEG=OFF -D WITH_JAVA=ON ../../..
