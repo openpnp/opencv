b0d4f462e7563b56f14a75e54e1b999cbcefd17c
Roman Valls Guimera <brainstorm@nopcode.org>
2020-05-14 23:33:37 +1000

    Minor fixes (permission denied), tidy up, debug

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 MACHINE_NAME=`uname -m`
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/$MACHINE_NAME-linux-gnu/jni
+export LD_LIBRARY_PATH=/usr/lib/$MACHINE_NAME-linux-gnu/jni
 export JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto
 export ANT_HOME=/usr/bin/ant
 
@@ -9,35 +9,36 @@ echo "Building for OpenCV $OPENCV_VERSION-$OPENCV_OPNP_REV on: \n `uname -a` \n"
 
 # Apply JNI Debian hacks, avoids OpenCV's cmake going:
 # "Could NOT find JNI (missing: JAVA_AWT_LIBRARY JAVA_JVM_LIBRARY JAVA_INCLUDE_PATH ...)"
-ln -sf /usr/lib/$MACHINE_NAME-linux-gnu/jni/libjnidispatch.system.so \
-	   /usr/lib/$MACHINE_NAME-linux-gnu/jni/libjnidispatch.so
+ls -alh /usr/lib/*-gnu/jni/*
+sudo ln -sf /usr/lib/$MACHINE_NAME-linux-gnu/jni/libjnidispatch.system.so \
+            /usr/lib/$MACHINE_NAME-linux-gnu/jni/libjnidispatch.so
 
 # Get and install a proper JVM
 wget https://corretto.aws/downloads/latest/amazon-corretto-11-$MACHINE_NAME-linux-jdk.deb \
-	 && dpkg -i *.deb \
-	 && rm *.deb
+     && dpkg -i *.deb \
+     && rm *.deb
 
 # XXX: ARMv8 for conditional folder detection
 cd opencv/opencv-$OPENCV_VERSION/target/linux/ARMv8
-cmake	-D BUILD_SHARED_LIBS=OFF \
-		-D BUILD_TESTING_SHARED=OFF \
-		-D BUILD_TESTING_STATIC=OFF \
-		-D BUILD_TESTING=OFF \
-		-D BUILD_EXAMPLES=OFF \
-		-D BUILD_DOCS=OFF \
-		-D INSTALL_C_EXAMPLES=OFF \
-		-D INSTALL_PYTHON_EXAMPLES=OFF \
-		-D WITH_EIGEN=OFF \
-		-D WITH_FFMPEG=OFF \
-		-D WITH_JAVA=ON ../../..
+cmake -D BUILD_SHARED_LIBS=OFF \
+      -D BUILD_TESTING_SHARED=OFF \
+      -D BUILD_TESTING_STATIC=OFF \
+      -D BUILD_TESTING=OFF \
+      -D BUILD_EXAMPLES=OFF \
+      -D BUILD_DOCS=OFF \
+      -D INSTALL_C_EXAMPLES=OFF \
+      -D INSTALL_PYTHON_EXAMPLES=OFF \
+      -D WITH_EIGEN=OFF \
+      -D WITH_FFMPEG=OFF \
+      -D WITH_JAVA=ON ../../..
 make -j8
 
 echo "Copy OpenCV resources\n"
 pwd && ls -alh
 cd ../../.. \
-	&& pwd && ls -alh \
-	&& find / -type f -iname "copy-resources.sh" \
-	&& find /usr/lib -type f -iname "libjnidispatch*.*" \
-	&& ./copy-resources.sh $OPENCV_VERSION
+   && pwd && ls -alh \
+   && find / -type f -iname "copy-resources.sh" \
+   && find /usr/lib -type f -iname "libjnidispatch*.*" \
+   && ./copy-resources.sh $OPENCV_VERSION
 
 mvn clean test
