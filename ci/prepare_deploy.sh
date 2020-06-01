251daea9c2b605beb84c611f453f98a63e12311e
Roman Valls Guimera <brainstorm@nopcode.org>
2020-06-02 00:55:06 +1000

    Fixup pre-deploy script, should substitute copy-resources.sh at this point

--- ci/prepare_deploy.sh
+++ ci/prepare_deploy.sh
@@ -10,17 +10,17 @@ mkdir -p $DEPLOY
 
 if [ $TRAVIS_OS_NAME = osx ]
 then
-	cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/osx/x86_64/lib/libopencv_java430.dylib $DEPLOY
+	cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/$TRAVIS_OS_NAME/$TRAVIS_CPU_ARCH/lib/libopencv_java430.dylib $DEPLOY
 fi
 
 if [ $TRAVIS_OS_NAME = linux ]
 then
 	case "$TRAVIS_CPU_ARCH" in
 		arm64)
-			cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/linux/ARMv8/lib/libopencv_java430.so $DEPLOY
+			cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/$TRAVIS_OS_NAME/$TRAVIS_CPU_ARCH/lib/libopencv_java430.so $DEPLOY
 		;;
-		x86_64)
-			cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/linux/x86_64/lib/libopencv_java430.so $DEPLOY
+		amd64)
+			cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/$TRAVIS_OS_NAME/$TRAVIS_CPU_ARCH/lib/libopencv_java430.so $DEPLOY
 		;;
 	esac
 fi
@@ -28,11 +28,11 @@ fi
 if [ $TRAVIS_OS_NAME = windows ]
 then
 	case "$TRAVIS_CPU_ARCH" in		
-		x86_64)
-			cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/linux/x86_64/lib/libopencv_java430.dll $DEPLOY
+		amd64)
+			cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/$TRAVIS_OS_NAME/$TRAVIS_CPU_ARCH/lib/libopencv_java430.dll $DEPLOY
 		;;
 #		x86_32)
-#			cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/linux/ARMv8/lib/libopencv_java430.so $DEPLOY
+#			cp $TRAVIS_BUILD_DIR/$TARGET_PREFIX/$TRAVIS_OS_NAME/$TRAVIS_CPU_ARCH/lib/libopencv_java430.so $DEPLOY
 #		;;
 	esac
 fi
